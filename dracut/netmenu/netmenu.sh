3925c175a126566045290445065b6eb0cbd056a4
hervyqa <hervyqa@protonmail.com>
2020-12-05 19:57:34 +0700

    add: fix link

--- dracut/netmenu/netmenu.sh
+++ dracut/netmenu/netmenu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dialog --colors --keep-tite --no-shadow --no-mouse \
-       --backtitle "\Zb\Z7LangitKetujuh installation -- https://langitketujuh.id/os\Zn" \
+       --backtitle "\Zb\Z7LangitKetujuh installation -- https://langitketujuh.id/\Zn" \
        --cancel-label "Reboot" --aspect 20 \
        --menu "Select an Action:" 10 50 2 \
        "Install" "Run langitketujuh-install" \
