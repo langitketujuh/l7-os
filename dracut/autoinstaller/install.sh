f9dccf67f55fc0746b73d1dccb572153aaa7c8e1
Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>
2022-04-08 09:52:14 +0700

    back to alpha.de.repo

--- dracut/autoinstaller/install.sh
+++ dracut/autoinstaller/install.sh
@@ -202,10 +202,10 @@ VAI_configure_autoinstall() {
     XBPS_ARCH="$(xbps-uhelper arch)"
     case $XBPS_ARCH in
         *-musl)
-            xbpsrepository="https://void.webconverger.org/current/musl"
+            xbpsrepository="https://alpha.de.repo.voidlinux.org/current/musl"
             ;;
         *)
-            xbpsrepository="https://void.webconverger.org/current"
+            xbpsrepository="https://alpha.de.repo.voidlinux.org/current"
             ;;
     esac
 
