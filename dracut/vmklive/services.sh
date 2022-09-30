1837f51a74d1c8534fb821457746c1ebf8181d0e
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2022-09-30 16:11:08 +0700

    fix openntpd service for installer

--- dracut/vmklive/services.sh
+++ dracut/vmklive/services.sh
@@ -22,7 +22,7 @@ done
 for f in $SERVICEDIR/*; do
     _service=${f##*/}
     case "${_service}" in
-        acpid|adb|agetty-console|agetty-generic|agetty-hv*|agetty-serial|agetty-tty[SAU]*|agetty-tty3|agetty-tty4|agetty-tty5|agetty-tty6|alsa|avahi-daemon|boltd|colord|cups-browsed|dhcpcd-*|dmeventd|elogind|fancontrol|gpsd|iptables|ip6tables|isc-ntpd|lvmetad|mysqld|mdadm|openntpd|pipewire|pipewire-pulse|pulseaudio|polkitd|rsyncd|rtkit|slapd|smartd|sshd|sulogin|svnserve|tcsd|wpa_supplicant) ;; # ignored
+        acpid|adb|agetty-console|agetty-generic|agetty-hv*|agetty-serial|agetty-tty[SAU]*|agetty-tty3|agetty-tty4|agetty-tty5|agetty-tty6|alsa|avahi-daemon|boltd|colord|cups-browsed|dhcpcd-*|dmeventd|elogind|fancontrol|gpsd|iptables|ip6tables|isc-ntpd|lvmetad|mysqld|mdadm|pipewire|pipewire-pulse|pulseaudio|polkitd|rsyncd|rtkit|slapd|smartd|sshd|sulogin|svnserve|tcsd|wpa_supplicant) ;; # ignored
         dhcpcd) [ -n "$dhcpcd" ] && ln -sf ${f##$NEWROOT} $NEWROOT/etc/runit/runsvdir/default/;;
         *) ln -sf ${f##$NEWROOT} $NEWROOT/etc/runit/runsvdir/default/;;
     esac
