1a961d851ace70ac72c86b950c83f6be02dbbb83
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-03-04 07:42:27 +0700

    remove colord group

--- dracut/vmklive/adduser.sh
+++ dracut/vmklive/adduser.sh
@@ -24,7 +24,7 @@ if ! grep -q ${USERSHELL} ${NEWROOT}/etc/shells ; then
 fi
 
 # Create new user and remove password. We'll use autologin by default.
-chroot ${NEWROOT} useradd -m -s $USERSHELL -c "$USERCOMMENT" -G _pipewire,audio,bluetooth,colord,video,wheel -s $USERSHELL $USERNAME
+chroot ${NEWROOT} useradd -m -s $USERSHELL -c "$USERCOMMENT" -G _pipewire,audio,bluetooth,video,wheel -s $USERSHELL $USERNAME
 chroot ${NEWROOT} passwd -d $USERNAME >/dev/null 2>&1
 
 # Setup default root/user password (langitketujuh).
