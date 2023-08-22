f76496afbbc1d521aed491dbfe63df57223d03d1
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-08-22 17:46:39 +0700

    various changes iso builder
    * merger packages to one file
    * edition: only base, home and studio
    * switch to langitketujuh-installer

--- build-x86-images.sh
+++ build-x86-images.sh
@@ -6,44 +6,13 @@ set -eu
 
 PROGNAME=$(basename "$0")
 ARCH=$(uname -m)
-IMAGES="base kde-home kde-studio kde-dev kde-datasc sway-home sway-studio sway-dev sway-datasc"
+IMAGES="base kde-home kde-studio"
 TRIPLET=
-DATE=$(date -u +%Y%m%d)
-GEN=$(pwgen -sA 7 1)
-REMOVE=
 REPO=
-
-# base packages
-PKGS_BASE="$(grep '^[^#].' pkg/base.txt | xargs)"
-PKGS_DESKTOP="$(grep '^[^#].' pkg/desktop.txt | xargs)"
-PKGS_KDE="$(grep '^[^#].' pkg/kde.txt | xargs)"
-PKGS_SWAY="$(grep '^[^#].' pkg/sway.txt | xargs)"
-PKGS_NONFREE="$(grep '^[^#].' pkg/nonfree.txt | xargs)"
-
-# gui apps packages
-PKGS_HOME="$(grep '^[^#].' pkg/home.txt | xargs)"
-PKGS_STUDIO="$(grep '^[^#].' pkg/studio.txt | xargs)"
-PKGS_DEV="$(grep '^[^#].' pkg/dev.txt | xargs)"
-PKGS_DATASC="$(grep '^[^#].' pkg/datasc.txt | xargs)"
-
-# remove packages after install
-PKGS_REMOVE="$(grep '^[^#].' pkg/remove.txt | xargs)"
-
-# variant packages
-# kde plasma
-PKGS_KDE_HOME="$PKGS_BASE $PKGS_DESKTOP $PKGS_HOME $PKGS_KDE"
-PKGS_KDE_STUDIO="$PKGS_KDE_HOME $PKGS_STUDIO"
-PKGS_KDE_DEV="$PKGS_KDE_HOME $PKGS_DEV"
-PKGS_KDE_DATASC="$PKGS_KDE_HOME $PKGS_DATASC"
-
-# sway
-PKGS_SWAY_HOME="$PKGS_BASE $PKGS_DESKTOP $PKGS_SWAY"
-PKGS_SWAY_STUDIO="$PKGS_SWAY_HOME $PKGS_STUDIO"
-PKGS_SWAY_DEV="$PKGS_SWAY_HOME $PKGS_DEV"
-PKGS_SWAY_DATASC="$PKGS_SWAY_HOME $PKGS_DATASC"
+DATE=$(date -u +%Y%m%d)
 
 help() {
-    echo "${0#/*}: [-a arch] [-b base|kde-home|kde-studio|kde-dev|kde-datasc|sway-home|sway-studio|sway-dev|sway-datasc] [-d date] [-t arch-date-variant] [-r repo] [-R rmpkgs]" >&2
+    echo "$PROGNAME: [-a arch] [-b base|kde-home|kde-studio] [-d date] [-t arch-date-variant] [-r repo]" >&2
 }
 
 while getopts "a:b:d:t:hr:V" opt; do
@@ -53,7 +22,6 @@ case $opt in
     d) DATE="$OPTARG";;
     h) help; exit 0;;
     r) REPO="-r $OPTARG $REPO";;
-    R) REMOVE="$OPTARG";;
     t) TRIPLET="$OPTARG";;
     V) version; exit 0;;
     *) help; exit 1;;
@@ -71,89 +39,72 @@ cleanup() {
 build_variant() {
     variant="$1"
     shift
-    IMG=langitketujuh-${variant}-${ARCH}-${DATE}-${GEN}.iso
-    SERVICES="acpid dbus earlyoom ntpd ufw uuidd zramen"
+    IMG=langitketujuh-live-${ARCH}-${DATE}-${variant}.iso
+    GRUB_PKGS="grub-i386-efi grub-x86_64-efi"
+    A11Y_PKGS="espeakup void-live-audio brltty"
+    LTS_PKGS="linux-lts linux-lts-headers"
+    FW_PKGS="alsa-firmware linux linux-base linux-headers sof-firmware"
+    TOOLS_PKGS="acpi age bluez chezmoi clinfo cryfs curl dbus-elogind dbus-elogind-libs dbus-elogind-x11 diffutils dracut dracut-network dracut-uefi earlyoom elogind encfs exfat-utils f2fs-tools fastfetch fig2dev fish-shell fontconfig gocryptfs gopass gptfdisk htop ifuse inetutils inxi iwd librsvg-utils libwebp-tools nano ntfs-3g ntp opendoas optipng pass pinentry pkg-config polkit rnnoise rsv sof-tools tmate tmux udisks2 ufw vim vpsm wget xtools yank zramen"
+    NONFREE_PKGS="void-repo-nonfree cnijfilter2"
+
+    BASE_L7_PKGS="l7-repo l7-fish-shell l7-opendoas l7-removed-packages l7-runit-void l7-shadow l7-tools"
+    BASE_PKGS="dialog cryptsetup lvm2 mdadm xtools-minimal xmirror $GRUB_PKGS $A11Y_PKGS $LTS_PKGS $FW_PKGS $TOOLS_PKGS $NONFREE_PKGS $BASE_L7_PKGS"
+
+    AC_PKGS="bsdtar 7zip 7zip-unrar unrar unzip xz zip zstd zutils"
+    CODEC_PKGS="alsa-pipewire alsa-utils bluez-alsa gstreamer1-pipewire libjack-pipewire pipewire"
+    BASEG_PKGS="android-tools android-udev-rules flatpak gnome-keyring wayclip wayland-utils xclip"
+    XF86_PKGS="xf86-input-evdev xf86-input-joystick xf86-input-libinput xf86-input-mtrack xf86-input-synaptics xf86-input-vmmouse xf86-input-wacom"
+    XORG_PKGS="xorg-minimal xorg-input-drivers xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf orca"
+    VULKAN_PKGS="Vulkan-Headers Vulkan-Tools Vulkan-ValidationLayers amdvlk libspa-vulkan mesa-vulkan-overlay-layer vkBasalt"
+    KDE_PKGS="appmenu-gtk3-module colord-kde ffmpegthumbs kde5 kdegraphics-thumbnailers kidentitymanagement kimageformats kio-gdrive ksuperkey libappindicator pinentry-qt plasma-applet-active-window-control plasma-disks plasma-firewall plasma-nm plasma-pa plasma-vault plasma-wayland-protocols qt5-imageformats qt6-wayland sddm konsole dolphin"
+    KDEA_PKGS="ark dragon-player elisa gnupg2-scdaemon gwenview kamoso kcalc kcm-wacomtablet kde5-baseapps kdeconnect kfind kgpg kmediaplayer krdc krename krfb ktorrent kwalletmanager okular partitionmanager print-manager skanpage spectacle sweeper"
+    FONT_PKGS="amiri-font font-adobe-source-code-pro font-adobe-source-sans-pro-v2 font-adobe-source-serif-pro font-awesome font-awesome5 font-awesome6 font-crosextra-caladea-ttf font-crosextra-carlito-ttf font-liberation-narrow-ttf noto-fonts-emoji ttf-opensans"
+    PRINTER_PKGS="bluez-cups brother-brlaser cups cups-filters cups-pdf cups-pk-helper epson-inkjet-printer-escpr foomatic-db foomatic-db-nonfree gutenprint hplip system-config-printer system-config-printer-udev"
+    DESKTOP_PKGS="firefox libreoffice libreoffice-kde qownnotes octoxbps corectrl inkscape gimp qpwgraph ssr"
+    XDG_PKGS="xdg-desktop-portal xdg-desktop-portal-kde xdg-user-dirs"
+    COSM_PKGS="papirus-icon-theme"
+
+    KDE_L7_PKGS="l7-krunner l7-appmenu-gtk3-module l7-ark l7-baloo5 l7-base-files l7-breeze l7-breeze-gtk l7-breeze-icons l7-desktop-file-utils l7-gwenview l7-kate5 l7-kcmutils l7-konsole l7-kscreenlocker l7-kservice l7-kwin l7-plasma-desktop l7-plasma-framework l7-plasma-workspace-wallpapers l7-sddm l7-systemsettings"
+    HOME_L7_PKGS="l7-dconf l7-papirus-icon-theme l7-site l7-wiki runit-backlight l7-export l7-gimp l7-inkscape l7-libreoffice l7-octoxbps l7-pipewire l7-qownnotes l7-ssr ccc isoimagewriter webapp-manager"
+    HOME_PKGS="$AC_PKGS $CODEC_PKGS $BASEG_PKGS $XF86_PKGS $XORG_PKGS $VULKAN_PKGS $KDE_PKGS $KDEA_PKGS $FONT_PKGS $PRINTER_PKGS $DESKTOP_PKGS $XDG_PKGS $COSM_PKGS $KDE_L7_PKGS $HOME_L7_PKGS"
+
+    STD_CODEC_PKGS="alsa-plugins-ffmpeg cdparanoia flac fluidsynth gst-plugins-ugly1 gstreamer-vaapi libspa-bluetooth libspa-jack mplayer opus-tools speex timidity twolame vorbis-tools vorbisgain wavpack"
+    STD_PLUGIN_PKGS="gimp-lqr-plugin gmic gmic-gimp inkscape-generate-palette inkscape-incadiff inkscape-inx-pathops inkscape-nextgenerator resynthesizer"
+    STD_AUDIO_PKGS="Carla ardour audacity cadence calf freepats jack_capture kid3 lmms python3-rdflib qtractor soundkonverter"
+    STD_AUDIOP_PKGS="abGate artyfx invada-studio-plugins invada-studio-plugins-lv2 swh-lv2 x42-plugins"
+    STD_VIDEOED_PKGS="obs handbrake kdenlive mediainfo mkvtoolnix-gui"
+    STD_ANIM_PKGS="goxel blender opentoonz synfigstudio"
+    STD_PHOTOG_PKGS="Converseen digikam rawtherapee"
+    STD_FONT_PKGS="font-inter google-fonts-ttf"
+    STD_PROD_PKGS="LibreCAD freecad openscad"
+    STD_LAY_PKGS="python3-tkinter scribus"
+    STD_PAINT_PKGS="gmic-krita krita"
+    STD_KEY_PKGS="screenkey slop"
+    STD_FONTM_PKGS="fontforge"
+    STD_CMON_PKGS="dispcalGUI"
+    STD_PROMP_PKGS="qprompt"
+    STD_PANO_PKGS="hugin"
+    STD_GAME_PKGS="godot"
+    STD_L7_PKGS="l7-ardour l7-audacity l7-blender l7-carla l7-godot l7-goxel l7-krita l7-openscad l7-opentoonz l7-scribus"
+
+    STUDIO_PKGS="$STD_CODEC_PKGS $STD_PLUGIN_PKGS $STD_AUDIO_PKGS $STD_AUDIOP_PKGS $STD_VIDEOED_PKGS $STD_ANIM_PKGS $STD_PHOTOG_PKGS $STD_FONT_PKGS $STD_PROD_PKGS $STD_LAY_PKGS $STD_PAINT_PKGS $STD_KEY_PKGS $STD_FONTM_PKGS $STD_CMON_PKGS $STD_PROMP_PKGS $STD_PANO_PKGS $STD_GAME_PKGS $STD_L7_PKGS"
+
+    SERVICES="sshd"
 
     LIGHTDM_SESSION=''
 
     case $variant in
         base)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_BASE"
-            else
-                PKGS="$PKGS_BASE"
-            fi
+            PKGS="$BASE_PKGS"
             SERVICES="$SERVICES dhcpcd wpa_supplicant acpid"
         ;;
         kde-home)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_KDE_HOME $PKGS_NONFREE"
-            else
-                PKGS="$PKGS_KDE_HOME"
-            fi
-            SERVICES="$SERVICES NetworkManager backlight bluetoothd bluez-alsa cupsd colord elogind sddm"
+            PKGS="$BASE_PKGS $HOME_PKGS"
+            SERVICES="$SERVICES NetworkManager bluetoothd bluez-alsa cupsd dbus earlyoom ntpd sddm ufw zramen"
         ;;
         kde-studio)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_KDE_STUDIO $PKGS_NONFREE"
-            else
-                PKGS="$PKGS_KDE_STUDIO"
-            fi
-            SERVICES="$SERVICES NetworkManager backlight bluetoothd bluez-alsa cupsd colord elogind sddm"
-        ;;
-        kde-dev)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_KDE_DEV $PKGS_NONFREE"
-                REMOVE="$PKGS_REMOVE"
-            else
-                >&2 echo "Not support architecture $ARCH"
-                exit 1
-            fi
-            SERVICES="$SERVICES NetworkManager backlight bluetoothd bluez-alsa cupsd colord elogind sddm"
-        ;;
-        kde-datasc)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_KDE_DATASC $PKGS_NONFREE"
-                REMOVE="$PKGS_REMOVE"
-            else
-                >&2 echo "Not support architecture $ARCH"
-                exit 1
-            fi
-            SERVICES="$SERVICES NetworkManager backlight bluetoothd bluez-alsa cupsd colord elogind sddm"
-        ;;
-        sway-home)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_SWAY_HOME $PKGS_NONFREE"
-            else
-                PKGS="$PKGS_SWAY_HOME"
-            fi
-            SERVICES="$SERVICES dhcpcd iwd greetd polkitd"
-        ;;
-        sway-studio)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_SWAY_STUDIO $PKGS_NONFREE"
-            else
-                PKGS="$PKGS_SWAY_STUDIO"
-            fi
-            SERVICES="$SERVICES dhcpcd iwd greetd polkitd"
-        ;;
-        sway-dev)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_SWAY_DEV $PKGS_NONFREE"
-            else
-                >&2 echo "Not support architecture $ARCH"
-                exit 1
-            fi
-            SERVICES="$SERVICES dhcpcd iwd greetd polkitd"
-        ;;
-        sway-datasc)
-            if [ "$ARCH" = x86_64 ]; then
-                PKGS="$PKGS_SWAY_DATASC $PKGS_NONFREE"
-            else
-                >&2 echo "Not support architecture $ARCH"
-                exit 1
-            fi
-            SERVICES="$SERVICES dhcpcd iwd greetd polkitd"
+            PKGS="$BASE_PKGS $HOME_PKGS $STUDIO_PKGS"
+            SERVICES="$SERVICES NetworkManager bluetoothd bluez-alsa cupsd dbus earlyoom ntpd sddm ufw zramen"
         ;;
         *)
             >&2 echo "Unknown variant $variant"
@@ -166,7 +117,7 @@ build_variant() {
         echo "$LIGHTDM_SESSION" > "$INCLUDEDIR"/etc/lightdm/.session
     fi
 
-    ./mklive.sh -a "$ARCH" -o "$IMG" -p "$PKGS" -R "$REMOVE" -S "$SERVICES" -I "$INCLUDEDIR" ${REPO} "$@"
+    ./mklive.sh -a "$ARCH" -o "$IMG" -p "$PKGS" -S "$SERVICES" -I "$INCLUDEDIR" ${REPO} "$@"
 }
 
 if [ ! -x mklive.sh ]; then
@@ -178,7 +129,7 @@ if [ -x installer.sh ]; then
     MKLIVE_VERSION="$(PROGNAME='' version)"
     installer=$(mktemp)
     sed "s/@@MKLIVE_VERSION@@/${MKLIVE_VERSION}/" installer.sh > "$installer"
-    install -Dm755 "$installer" "$INCLUDEDIR"/usr/bin/langitketujuh-install
+    install -Dm755 "$installer" "$INCLUDEDIR"/usr/bin/langitketujuh-installer
     rm "$installer"
 else
     echo installer.sh not found >&2
